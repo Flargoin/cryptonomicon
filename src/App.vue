da4a6d915dc2d8a12b29dd1f6ac0975f6b3bc69f
Романов Сергей Сергеевич <RomanovSS@plast.local>
2023-11-22 08:39:35 +0300

    Добавил localStorage(Нужно поправить сохранение последнего элемента)

--- src/App.vue
+++ src/App.vue
@@ -140,6 +140,15 @@ export default {
   },
 
   created: function() {
+    const tickersData = localStorage.getItem("cryptonomicon-list");
+
+    if(tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach(ticker => {
+        this.subscribeToUpdates(ticker.name);
+      });
+    }
+
       fetch(`https://min-api.cryptocompare.com/data/all/coinlist?summary=true`)
       .then(response => response.json())
       .then(data => {
@@ -150,24 +159,28 @@ export default {
   },
 
   methods: {
-    add() {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=0c77fb07775d90b64cfdc69af9642117d0c35c877b49f80b430ae062ceedea42`)
 
+        const data = await f.json();
+
+        this.tickers.find(t => t.name === tickerName).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+
+        if(this.sel?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      } ,3000);
+    },
+
+    add() {
       const currentTicker = {
         name: this.ticker,
         price: "-"
       };
 
-      setInterval(async () => {
-        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=0c77fb07775d90b64cfdc69af9642117d0c35c877b49f80b430ae062ceedea42`)
-
-        const data = await f.json();
-
-        this.tickers.find(t => t.name === currentTicker.name).price = data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-
-        if(this.sel?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      } ,3000);
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name);
 
       this.tickers.push(currentTicker);
       this.ticker = '';
